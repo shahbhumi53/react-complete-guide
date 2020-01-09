2cf0bc170971e000a863fce9d2996ef64991736f
shabhumi53 <shabhumi53@gmail.com>
2020-01-09 15:22:55 +0530

    re-render DOM on state or prop change

--- src/App.js
+++ src/App.js
@@ -27,24 +27,58 @@ import Person from "./Person/Person";
 
 // ----------------- CLASS BASE DECLARATION OF COMPONENT ---------------//
 class App extends Component {
+
+    /*
+    - as props contains something that is comes from outside of the component
+    - state is something that is managed inside the component
+    - state is available only in components that are created by extending *Component* class of
+    react
+    - state is the reserved word
+    - SPECIAL CASE : state can be changed and if it changes then it re-render the DOM
+    - react dom will be re-rendered only in following 2 cases
+    - if state changes or prop changes then react will re-render or update the DOM
+    */
     state = {
         persons: [
             { name: "Dhwani", age: 25 },
             { name: "Vaidehi", age: 27 },
-            { name: "Beena", age: 29 },
-        ]
+            { name: "Beena", age: 10 },
+        ],
+        otherStateProp: 'Will it be changed ?'
     };
+
+    switchNameHandler = () => {
+        // console.log('Hello');
+        // DONT'T DO THIS - this.state.persons[0] = "DJ";
+        this.setState({
+            persons: [
+                { name: "DJ", age: 25 },
+                { name: "Vaidehi", age: 27 },
+                { name: "Beena", age: 48 },
+            ]
+        })
+    };
+
     render() {
         return (
             <div className="App">
                 <h1>Hi I'm a react APP</h1>
                 <p>This is really working</p>
-                <button>Switch names</button>
-                <Person name="Dhwani" age="21" />
+
+                {/* make sure not to use round bracket like this.switchNameHandler() as it will
+                 execute that functional immediately on load of the page. But we need the
+                  function to be executed only when button is clicked. We just passed the
+                   reference */}
+                <button onClick={ this.switchNameHandler } >Switch names</button>
+
+                <br/>
+                <b>{ this.state.otherStateProp }</b>
+
+                <Person name={ this.state.persons[0].name } age={ this.state.persons[0].age } />
                 <hr/>
-                <Person name="Vaidehi" age="23" >My hobby is : Shoes... collecting shoes</Person>
+                <Person name={ this.state.persons[1].name } age={ this.state.persons[1].age } >My hobby is : Shoes... collecting shoes</Person>
                 <hr/>
-                <Person name="Beena" age="22" />
+                <Person name={ this.state.persons[2].name } age={ this.state.persons[2].age } />
             </div>
         );
     }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
+/*
+ - as props contains something that is comes from outside of the component
+ - state is something that is managed inside the component
+ - react dom will be re-rendered only in following 2 cases
+ - if state changes or prop changes then react will re-render or update the DOM
+ */
 const person = (props) => {
     // dynamic content can be part of JSX, just need to wrap it on curly braces
     /*this will render anything between tag of Person in JSX */
     return (
         <div>
-            <p>I am a { props.name } and { Math.floor(Math.random() * 30) } year old.</p>
+            {/*<p>I am a { props.name } and { Math.floor(Math.random() * 30) } year old.</p>*/}
+            <p>I am a { props.name } and { props.age } year old.</p>
             <p>{ props.children }</p>
         </div>
     );
