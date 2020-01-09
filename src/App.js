9f37535977f449f2c8e8370f39d5486bd8d39157
shabhumi53 <shabhumi53@gmail.com>
2020-01-09 14:45:40 +0530

    function base and class base declaration of the component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 import Person from "./Person/Person";
 
-function App() {
-    return (
-        // this is actually call JSX
-        // JSX is just syntactic sugar for JavaScript, allowing you to write
-        // HTMLish code instead of nested React.createElement(...) calls
-        <div className="App">
-            <h1>Hi I'm a react APP</h1>
-            <p>This is really working</p>
-            <Person name="Dhwani" age="21" />
-            <Person name="Vaidehi" age="23" />
-            <Person name="Beena" age="22" />
-        </div>
-    );
-    // things behind this works like this, that is why in above syntax we
-    // do not use React but still import it
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null,  'Hi I\'m a react APP'));
+// ----------------- FUNCTION BASE DECLARATION OF COMPONENT ---------------//
+// function App() {
+//     return (
+//         // this is actually call JSX
+//         // JSX is just syntactic sugar for JavaScript, allowing you to write
+//         // HTMLish code instead of nested React.createElement(...) calls
+//         <div className="App">
+//             <h1>Hi I'm a react APP</h1>
+//             <p>This is really working</p>
+//             <Person name="Dhwani" age="21" />
+//             <hr/>
+//             <Person name="Vaidehi" age="23" >My hobby is : Shoes... collecting shoes</Person>
+//             <hr/>
+//             <Person name="Beena" age="22" />
+//         </div>
+//     );
+//     // things behind this works like this, that is why in above syntax we
+//     // do not use React but still import it
+//     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null,  'Hi I\'m a react APP'));
+// }
+
+
+// ----------------- CLASS BASE DECLARATION OF COMPONENT ---------------//
+class App extends Component {
+    state = {
+        persons: [
+            { name: "Dhwani", age: 25 },
+            { name: "Vaidehi", age: 27 },
+            { name: "Beena", age: 29 },
+        ]
+    };
+    render() {
+        return (
+            <div className="App">
+                <h1>Hi I'm a react APP</h1>
+                <p>This is really working</p>
+                <button>Switch names</button>
+                <Person name="Dhwani" age="21" />
+                <hr/>
+                <Person name="Vaidehi" age="23" >My hobby is : Shoes... collecting shoes</Person>
+                <hr/>
+                <Person name="Beena" age="22" />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 const person = (props) => {
     // dynamic content can be part of JSX, just need to wrap it on curly braces
-    return <p>I am a { props.name } and { Math.floor(Math.random() * 30) } year old.</p>;
+    /*this will render anything between tag of Person in JSX */
+    return (
+        <div>
+            <p>I am a { props.name } and { Math.floor(Math.random() * 30) } year old.</p>
+            <p>{ props.children }</p>
+        </div>
+    );
 };
 
 export default person;
