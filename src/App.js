cfa86a7f5e0729198e9b088626969b5d8a5a4ea0
shabhumi53 <shabhumi53@gmail.com>
2020-01-09 13:02:17 +0530

    added Person component, .idea folder added to gitignore

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
 
+import Person from "./Person/Person";
+
 function App() {
-  return (
-    <div className="App">
-      <h1>Hi I'm a react APP</h1>
-    </div>
-  );
+    return (
+        // this is actually call JSX
+        // JSX is just syntactic sugar for JavaScript, allowing you to write
+        // HTMLish code instead of nested React.createElement(...) calls
+        <div className="App">
+            <h1>Hi I'm a react APP</h1>
+            <p>This is really working</p>
+            <Person></Person>
+        </div>
+    );
+    // things behind this works like this, that is why in above syntax we
+    // do not use React but still import it
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null,  'Hi I\'m a react APP'));
 }
 
 export default App;
