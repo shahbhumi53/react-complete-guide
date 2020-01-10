0a6202680f513c3f47bb0622981e110d10504862
shabhumi53 <shabhumi53@gmail.com>
2020-01-10 14:38:00 +0530

    rendering content conditionally

--- src/App.js
+++ src/App.js
@@ -29,22 +29,23 @@ import Person from "./Person/Person";
 class App extends Component {
 
     /*
-    - as props contains something that is comes from outside of the component
-    - state is something that is managed inside the component
-    - state is available only in components that are created by extending *Component* class of
-    react
-    - state is the reserved word
-    - SPECIAL CASE : state can be changed and if it changes then it re-render the DOM
-    - react dom will be re-rendered only in following 2 cases
-    - if state changes or prop changes then react will re-render or update the DOM
-    */
+     - as props contains something that is comes from outside of the component
+     - state is something that is managed inside the component
+     - state is available only in components that are created by extending *Component* class of
+     react
+     - state is the reserved word
+     - SPECIAL CASE : state can be changed and if it changes then it re-render the DOM
+     - react dom will be re-rendered only in following 2 cases
+     - if state changes or prop changes then react will re-render or update the DOM
+     */
     state = {
         persons: [
             { name: "Dhwani", age: 25 },
             { name: "Vaidehi", age: 27 },
             { name: "Beena", age: 10 },
         ],
-        otherStateProp: 'Will it be changed ?'
+        otherStateProp: 'Will it be changed ?',
+        showPersons: false,
     };
 
     switchNameHandler = (args) => {
@@ -69,6 +70,11 @@ class App extends Component {
         });
     };
 
+    toggelPersons = () => {
+        const doesShow = this.state.showPersons;
+        this.setState({ showPersons: !doesShow });
+    };
+
     render() {
 
         // this is inline style
@@ -88,26 +94,30 @@ class App extends Component {
 
                 {/* make sure not to use round bracket like this.switchNameHandler() as it will
                  execute that functional immediately on load of the page. But we need the
-                  function to be executed only when button is clicked. We just passed the
-                   reference */}
-                {/*  following syntax is inconvenient  */}
+                 function to be executed only when button is clicked. We just passed the
+                 reference */}
+
                 <button style={ btnStyle }
-                        onClick={ () => this.switchNameHandler('DJ') } >Switch names</button>
+                        onClick={ this.toggelPersons } >Switch names</button>
 
-                {/*<br/>
-                <b>{ this.state.otherStateProp }</b>*/}
-
-                <Person name={ this.state.persons[0].name }
-                        age={ this.state.persons[0].age } />
-                {/* preferable syntax to declare click events  */}
-                <Person name={ this.state.persons[1].name }
-                        age={ this.state.persons[1].age }
-                        changed={ this.nameChangedHandler }
-                        click={ this.switchNameHandler.bind(this, 'DJOSHI') }>
-                    My hobby is : Shoes... collecting shoes
-                </Person>
-                <Person name={ this.state.persons[2].name }
-                        age={ this.state.persons[2].age } />
+                {/*
+                    a simple JSX expression that uses ternary operator
+                */}
+                { this.state.showPersons ?
+                    <div>
+                        <Person name={ this.state.persons[0].name }
+                                age={ this.state.persons[0].age } />
+                        {/* preferable syntax to declare click events  */}
+                        <Person name={ this.state.persons[1].name }
+                                age={ this.state.persons[1].age }
+                                changed={ this.nameChangedHandler }
+                                click={ this.switchNameHandler.bind(this, 'DJOSHI') }>
+                            My hobby is : Shoes... collecting shoes
+                        </Person>
+                        <Person name={ this.state.persons[2].name }
+                                age={ this.state.persons[2].age } />
+                    </div> : null
+                }
             </div>
         );
     }
