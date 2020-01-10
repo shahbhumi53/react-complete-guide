c52c06cc59d835b52adbfcacf6a20ef6de22ad84
shabhumi53 <shabhumi53@gmail.com>
2020-01-10 15:13:08 +0530

    output the list wil map function

--- src/App.js
+++ src/App.js
@@ -95,17 +95,11 @@ class App extends Component {
         if(this.state.showPersons) {
             persons = (
                 <div>
-                    <Person name={ this.state.persons[0].name }
-                            age={ this.state.persons[0].age } />
-                    {/* preferable syntax to declare click events  */}
-                    <Person name={ this.state.persons[1].name }
-                            age={ this.state.persons[1].age }
-                            changed={ this.nameChangedHandler }
-                            click={ this.switchNameHandler.bind(this, 'DJOSHI') }>
-                        My hobby is : Shoes... collecting shoes
-                    </Person>
-                    <Person name={ this.state.persons[2].name }
-                            age={ this.state.persons[2].age } />
+                    {
+                        this.state.persons.map((person) => {
+                            return <Person name={ person.name } age={ person.age } />
+                        })
+                    }
                 </div>
             );
         }
